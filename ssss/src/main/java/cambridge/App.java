c6e5bccb0e72351dabb1a073f4708d165e445b3d
kruthikam01 <kruthikam.22cse@cambridge.edu.in>
2025-06-09 17:15:41 +0530

    Azuredev

---
+++ ssss/src/main/java/cambridge/App.java
@@ -0,0 +1,13 @@
+package cambridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
